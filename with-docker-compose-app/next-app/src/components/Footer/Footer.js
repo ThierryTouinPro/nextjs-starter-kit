73eff135fceacd8ea18d5170fd91a38096dbd2c2
ZahirHaniche-dev <haniche.zahir@gmail.com>
2024-10-14 17:02:21 +0200

    ZHA: Code Review and correction Sprint1 Footer

--- with-docker-compose-app/next-app/src/components/Footer/Footer.js
+++ with-docker-compose-app/next-app/src/components/Footer/Footer.js
@@ -7,19 +7,20 @@ function Footer() {
                 <div className='container p-2'>
                     <div className='row mt-5 mb-5'> 
                         <div className='col-md-3 col-lg-2 col-xl-2 mx-auto'>
-                            <h4 class="fw-bold mb-4">Blogr</h4>  
+                            <h2 class="fw-bold mb-4 text-white">Blogr</h2>  
                         </div>
                         {mainMenus.map((footerMenu) => (
-                            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto" key={footerMenu.groupTitle}>
-                                <h6 className='fw-bold mb-3'>
+                            <div className="col-auto mx-auto" key={footerMenu.groupTitle}>
+                                <h4 className='fw-bold mb-3 text-white'>
                                     {footerMenu.groupTitle}
-                                </h6>
-                                {footerMenu.subMenus.map((subMenu) => (
-                                    <p className='mb-1'>
-                                        <a href={`/${subMenu}`}>{subMenu}</a>
-                                    </p>   
-                                ))}    
-                                        
+                                </h4>
+                                <ul className='list-unstyled text-white'>
+                                    {footerMenu.subMenus.map((subMenu) => (
+                                        <li className='mb-1' key={subMenu}>
+                                            <a href={`/${subMenu}`}>{subMenu}</a>
+                                        </li>   
+                                    ))}    
+                                </ul>
                             </div>
                         ))}
                     </div>
