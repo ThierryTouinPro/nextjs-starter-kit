d40b51a85228c33d5115a7ff3fa980ec56c00b3d
khouloud96 <khouloud.bensalem@ensi-uma.tn>
2024-10-24 11:15:13 +0200

    fix RadioGroup component to retain its state when navigating back and forth between steps

--- with-docker-compose-app/next-app/src/components/Interface/RadioGroup.js
+++ with-docker-compose-app/next-app/src/components/Interface/RadioGroup.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { useFormContext } from "react-hook-form";
-import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
+import { useFormContext, Controller } from "react-hook-form";
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+} from "@mui/material";
 
 export default function RadioGroupUI({ label, name, options, icon }) {
   const {
-    register,
+    control,
     formState: { errors },
   } = useFormContext();
 
@@ -13,29 +18,30 @@ export default function RadioGroupUI({ label, name, options, icon }) {
       <div className="d-flex align-items-center gap-3">
         {/* Label et icône alignés à gauche */}
         <label className="d-flex align-items-center gap-2">
-          <span className="d-flex align-items-center">{icon}</span> 
-          <span className="d-flex align-items-center mt-1">{label} :</span> 
+          <span className="d-flex align-items-center">{icon}</span>
+          <span className="d-flex align-items-center mt-1">{label} :</span>
           <span className="text-danger">*</span>
         </label>
-
         {/* RadioGroup pour gérer correctement la sélection */}
-        <RadioGroup row aria-label={label} name={name}>
-          {options.map((option) => (
-            <FormControlLabel
-              key={option.value}
-              control={
-                <Radio
-                  {...register(name, { required: "Ce champ est requis" })}
+        <Controller
+          name={name}
+          control={control}
+          rules={{ required: "Ce champ est requis" }}
+          render={({ field }) => (
+            <RadioGroup row {...field} value={field.value || ""}>
+              {options.map((option) => (
+                <FormControlLabel
+                  key={option.value}
                   value={option.value}
+                  control={<Radio />}
+                  label={option.label}
+                  className="d-flex align-items-center"
                 />
-              }
-              label={option.label}
-              className="d-flex align-items-center"
-            />
-          ))}
-        </RadioGroup>
+              ))}
+            </RadioGroup>
+          )}
+        />
       </div>
-
       {/* Affichage des erreurs */}
       {errors[name] && (
         <span className="error text-danger">{errors[name].message}</span>
