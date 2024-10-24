fd0ebc7062ce3823adc65c8921f3b7d854defbaf
ZahirHaniche-dev <haniche.zahir@gmail.com>
2024-10-24 10:49:47 +0200

    ZHA: Winston only server and dev

--- with-docker-compose-app/next-app/src/components/Authentification/Register/Registration.js
+++ with-docker-compose-app/next-app/src/components/Authentification/Register/Registration.js
@@ -55,7 +55,7 @@ export default function Registration() {
     console.log(finalData);
     console.log("Formatted Birth Date: ", formattedBirthDate);
 
-    /*try {
+    try {
       const response = await fetch("/api/auth/register", {
         method: "POST",
         headers: {
@@ -84,13 +84,14 @@ export default function Registration() {
     } catch (error) {
       console.error("Error during registration:", error);
       setError("global", { message: "Internal Server Error" });
-    }*/
+    }
   };
 
   return (
     <>
       <FormProvider {...methods}>
-        <form id="auth-form" onSubmit={handleSubmit(onSubmit)}>
+        {/* <form id="auth-form" onSubmit={handleSubmit(onSubmit)}> */}
+        <form id="auth-form" > 
           {step === 1 && (
             <>
               <RegisterInformation onNext={handleNextStep} />
--- with-docker-compose-app/next-app/src/pages/index.js
+++ with-docker-compose-app/next-app/src/pages/index.js
@@ -2,8 +2,8 @@ import Head from "next/head";
 import MainHome from "../components/Home/MainHome";
 
 let logger;
-if (typeof window === 'undefined') {
-  // Nous sommes dans un environnement côté serveur
+if (typeof window === 'undefined' && process.env.NODE_ENV === 'development') {
+  // Nous sommes dans un environnement côté serveur et en développement
   logger = require('../config/winston');
 }
 
@@ -22,7 +22,7 @@ export default function Home() {
 }
 
 export async function getServerSideProps() {
-  // Vérifiez si le logger est défini (cela signifie que nous sommes côté serveur)
+  // Vérifiez si le logger est défini (cela signifie que nous sommes côté serveur et en développement)
   if (logger) {
     logger.info("Page d'accueil rendue avec succès - Niveau Info");
     logger.error("Une erreur fictive est survenue - Niveau Error");
